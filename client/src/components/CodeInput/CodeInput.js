b4def925777010f35b87ad5f084ddc4d48106c81
Bharat Dangi <bharatdangi2074@gmail.com>
2021-12-23 10:33:23 +0545

    Some minor corrections

--- client/src/components/CodeInput/CodeInput.js
+++ client/src/components/CodeInput/CodeInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./codeInput.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -13,6 +13,7 @@ const CodeInput = () => {
     otp5: "",
     otp6: "",
   });
+  const [newClass, setNewClass] = useState("");
   const [error, setError] = useState(false);
 
   const { otp1, otp2, otp3, otp4, otp5, otp6 } = otpValues;
@@ -29,6 +30,38 @@ const CodeInput = () => {
         elmnt.target.form.elements[next].focus();
       }
     }
+  };
+  useEffect(() => {
+    const checkNumeric = (e) => {
+      if (
+        !/^\d+$/.test(otp1) ||
+        !/^\d+$/.test(otp2) ||
+        !/^\d+$/.test(otp3) ||
+        !/^\d+$/.test(otp4) ||
+        !/^\d+$/.test(otp5) ||
+        !/^\d+$/.test(otp6)
+      ) {
+        setNewClass("highlightClass");
+      } else {
+        setNewClass("");
+      }
+    };
+    checkNumeric();
+  }, [otp1, otp2, otp3, otp4, otp5, otp6]);
+
+  const handlePaste = (e) => {
+    const pastedData = e.clipboardData.getData("text");
+    const splitedData = pastedData.split("");
+    setOtpValues({
+      ...otpValues,
+      otp1: splitedData[0],
+      otp2: splitedData[1],
+      otp3: splitedData[2],
+      otp4: splitedData[3],
+      otp5: splitedData[4],
+      otp6: splitedData[5],
+    });
+    e.target.form.elements[5].focus();
   };
 
   const handleSubmit = async (e) => {
@@ -54,6 +87,10 @@ const CodeInput = () => {
     }
   };
 
+  const handleChange = (e) => {
+    setOtpValues({ ...otpValues, [e.target.name]: e.target.value });
+  };
+
   return (
     <form className="codeInputContainer">
       <p className="title">Verification Code:</p>
@@ -62,68 +99,86 @@ const CodeInput = () => {
           name="otp1"
           type="text"
           autoComplete="off"
-          className="otpInput"
+          className={`otpInput ${newClass}`}
           value={otp1}
-          onChange={(e) => setOtpValues({ ...otpValues, otp1: e.target.value })}
+          onChange={(e) => {
+            handleChange(e);
+          }}
           tabIndex="1"
           maxLength="1"
           onKeyUp={(e) => inputfocus(e)}
+          onPaste={(e) => handlePaste(e)}
         />
         <input
           name="otp2"
           type="text"
           autoComplete="off"
-          className="otpInput"
+          className={`otpInput ${newClass}`}
           value={otp2}
-          onChange={(e) => setOtpValues({ ...otpValues, otp2: e.target.value })}
+          onChange={(e) => {
+            handleChange(e);
+          }}
           tabIndex="2"
           maxLength="1"
           onKeyUp={(e) => inputfocus(e)}
+          onPaste={(e) => handlePaste(e)}
         />
         <input
           name="otp3"
           type="text"
           autoComplete="off"
-          className="otpInput"
+          className={`otpInput ${newClass}`}
           value={otp3}
-          onChange={(e) => setOtpValues({ ...otpValues, otp3: e.target.value })}
+          onChange={(e) => {
+            handleChange(e);
+          }}
           tabIndex="3"
           maxLength="1"
           onKeyUp={(e) => inputfocus(e)}
+          onPaste={(e) => handlePaste(e)}
         />
         <input
           name="otp4"
           type="text"
           autoComplete="off"
-          className="otpInput"
+          className={`otpInput ${newClass}`}
           value={otp4}
-          onChange={(e) => setOtpValues({ ...otpValues, otp4: e.target.value })}
+          onChange={(e) => {
+            handleChange(e);
+          }}
           tabIndex="4"
           maxLength="1"
           onKeyUp={(e) => inputfocus(e)}
+          onPaste={(e) => handlePaste(e)}
         />
 
         <input
           name="otp5"
           type="text"
           autoComplete="off"
-          className="otpInput"
+          className={`otpInput ${newClass}`}
           value={otp5}
-          onChange={(e) => setOtpValues({ ...otpValues, otp5: e.target.value })}
+          onChange={(e) => {
+            handleChange(e);
+          }}
           tabIndex="5"
           maxLength="1"
           onKeyUp={(e) => inputfocus(e)}
+          onPaste={(e) => handlePaste(e)}
         />
         <input
           name="otp6"
           type="text"
           autoComplete="off"
-          className="otpInput"
+          className={`otpInput ${newClass}`}
           value={otp6}
-          onChange={(e) => setOtpValues({ ...otpValues, otp6: e.target.value })}
+          onChange={(e) => {
+            handleChange(e);
+          }}
           tabIndex="6"
           maxLength="1"
           onKeyUp={(e) => inputfocus(e)}
+          onPaste={(e) => handlePaste(e)}
         />
       </div>
       {error && <p className="errorMessage">Verification Error</p>}
